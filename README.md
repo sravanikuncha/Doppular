660fcf54903a1ac69f585ccc9c8300f67386c30c
sravani <kunchasravani@gmail.com>
2025-02-10 18:14:11 +0530

    add routes

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # lookalike-backend
+
+vercel -new project did deployment
+mongodb allowed all ip address
--- index.js
+++ index.js
@@ -2,14 +2,24 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
-
-// middleware
-app.use(express.json());
-
 const login = require("./routes/login");
 const signup = require("./routes/signup");
 const code = require("./routes/code");
 const staticdata = require("./routes/staticdata");
+const settingsRouter=require('./routes/settings');
+const profileRouter=require('./routes/profile');
+const searchRouter=require('./routes/search');
+const alertRouter=require('./routes/alerts');
+
+const jwtAuth=require('./middleware/auth');
+const cookieParser=require('cookie-parser');
+
+
+// middleware
+app.use(express.json());
+
+
+app.use(cookieParser());
 
 mongoose
   .connect(process.env.DATABASE_URL, {
@@ -27,6 +37,11 @@ app.use("/api/login", login);
 app.use("/api/signup", signup);
 app.use("/api/code", code);
 app.use("/api/staticdata", staticdata);
+app.use("/api/settings",jwtAuth,settingsRouter);
+app.use("/api/profile",jwtAuth,profileRouter);
+app.use("/api/search",jwtAuth,searchRouter);
+app.use("/api/alerts",jwtAuth,alertRouter);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
